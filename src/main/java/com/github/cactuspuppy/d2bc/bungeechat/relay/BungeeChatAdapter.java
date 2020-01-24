a83ab6b4294327eff9a472388ce5d1ea34eb2b94
CactusPuppy <CactusPuppy@users.noreply.github.com>
2020-01-24 12:49:44 -0800

    BungeeChatAdapter: - Switch to polling logs for changes instead of WatchService - Add failsafes for preventing log spam DiscordAdapter: - Fixed double space error on player joining. Single and ready to mingle. - Added more descriptive logging for easier troubleshooting - Changed variable name to be more descriptive. Descripting not included.

--- src/main/java/com/github/cactuspuppy/d2bc/bungeechat/relay/BungeeChatAdapter.java
+++ src/main/java/com/github/cactuspuppy/d2bc/bungeechat/relay/BungeeChatAdapter.java
@@ -1,7 +1,6 @@
 package com.github.cactuspuppy.d2bc.bungeechat.relay;
 
 import com.github.cactuspuppy.d2bc.D2BC;
-import lombok.Getter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
@@ -13,19 +12,22 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
-
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
 public class BungeeChatAdapter {
     private static Pattern bungeeChatPattern = Pattern.compile("^\\[\\d{2}:\\d{2}:\\d{2}\\]: ([A-Z]+) > ([A-Za-z]+) > ([A-Za-z0-9_~]{3,16})\\([a-z0-9-]+\\): (.+)");
@@ -50,46 +52,61 @@ public class BungeeChatAdapter {
     private static BaseComponent[] discordPrefix = (new ComponentBuilder("[")).color(ChatColor.WHITE).bold(true)
             .append("Discord").color(ChatColor.AQUA).append("] ").color(ChatColor.WHITE).create();
 
+    private static String lastReadFilePath = "lastRead.dat";
+
     // Adapted from https://stackoverflow.com/a/40003648
     public BungeeChatAdapter(Path bungeeChatLogsPath) {
         //TODO: Load and save lastLineRead to disk
         lastLineRead = -1;
         logsPath = bungeeChatLogsPath;
-        if (!Files.isDirectory(logsPath))
-        // Get latest file (from
-        try {
-            Optional<Path> lastFilePath = Files.list(logsPath)    // here we get the stream with full directory listing
-                    .filter(f -> !Files.isDirectory(f))  // exclude subdirectories from listing
-                    .max(Comparator.comparingLong(f -> f.toFile().lastModified()));  // finally get the last file using simple comparator by lastModified field
-            if (!lastFilePath.isPresent()) {
-                D2BC.getPlugin().getLogger().info("No BungeeChat logs currently present");
-                latestLogPath = null;
-            } else {
-                latestLogPath = lastFilePath.get();
+        if (!Files.isDirectory(logsPath)) {
+            D2BC.getPlugin().getLogger().warning("BungeeChat logs folder does not exist! " +
+                    "Please ensure that you have the Logging module enabled in BungeeChat");
+            latestLogPath = null;
+        } else {
+            // Get latest file (from https://stackoverflow.com/a/30892976)
+            try {
+                Optional<Path> lastFilePath = Files.list(logsPath)    // here we get the stream with full directory listing
+                        .filter(f -> !Files.isDirectory(f))  // exclude subdirectories from listing
+                        .max(Comparator.comparingLong(f -> f.toFile().lastModified()));  // finally get the last file using simple comparator by lastModified field
+                if (!lastFilePath.isPresent()) {
+                    D2BC.getPlugin().getLogger().info("No BungeeChat logs currently present");
+                    latestLogPath = null;
+                } else {
+                    D2BC.getPlugin().getLogger().info("Loaded latest log file: " + lastFilePath.get().toString());
+                    latestLogPath = lastFilePath.get();
+                }
+            } catch (IOException e) {
+                D2BC.getPlugin().getLogger().log(Level.SEVERE, "Exception while finding latest log", e);
+                return;
             }
-        } catch (IOException e) {
-            D2BC.getPlugin().getLogger().log(Level.SEVERE, "Exception while finding latest log", e);
-            return;
         }
         // Schedule log check
         fileWatcher = ProxyServer.getInstance().getScheduler().schedule(D2BC.getPlugin(), processLogChanges,
-                0, 1000, TimeUnit.MILLISECONDS);
+                0, 100, TimeUnit.MILLISECONDS);
     }
 
     /**
      * Checks for new chat log changes
      */
+    @SuppressWarnings("FieldCanBeLocal")
     private Runnable processLogChanges = () -> {
         // Check for newer file
         LocalDateTime now = LocalDateTime.now();
-        Path newest = Paths.get(ProxyServer.getInstance().getPluginsFolder().getAbsolutePath(),
+        Path todayLog = Paths.get(ProxyServer.getInstance().getPluginsFolder().getAbsolutePath(),
                 File.separator, "BungeeChat", File.separator, "logs", File.separator,
                 String.format("%d-%02d-%02d-chat.log", now.getYear(), now.getMonthValue(), now.getDayOfMonth()));
-        if (!newest.equals(latestLogPath)) {
-            //Finish current log file
+        if (!Files.isRegularFile(todayLog)) {
+            D2BC.getPlugin().getLogger().fine("No chat file for today yet...");
+            return;
+        } else if (!todayLog.toAbsolutePath().equals(latestLogPath.toAbsolutePath())) {
+            D2BC.getPlugin().getLogger().info("Rolling logs to new day...");
+            D2BC.getPlugin().getLogger().info("Old log: " + latestLogPath.toAbsolutePath().toString());
+            D2BC.getPlugin().getLogger().info("New log: " + todayLog.toAbsolutePath().toString());
+            // Finish current log file
             relayNewMessages(latestLogPath);
             lastLineRead = -1;
-            latestLogPath = newest;
+            latestLogPath = todayLog;
         }
         relayNewMessages(latestLogPath);
     };
@@ -111,7 +128,7 @@ public class BungeeChatAdapter {
                 lastLineRead++;
             });
         } catch (NoSuchFileException e) {
-            D2BC.getPlugin().getLogger().log(Level.WARNING, "Could not find today's BungeeChat chat log", e);
+            D2BC.getPlugin().getLogger().fine("Could not find today's BungeeChat chat log");
         } catch (IOException e) {
             D2BC.getPlugin().getLogger().log(Level.SEVERE, "Exception while parsing log file", e);
         }
@@ -131,6 +148,7 @@ public class BungeeChatAdapter {
         builder.bold(true);
         // Append message
         builder.append(event.getMessage().getContentDisplay());
+        builder.bold(false);
         // Create final message
         BaseComponent[] message = builder.create();
 
